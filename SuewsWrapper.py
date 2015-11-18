eb62e5c4084efe27a118dca153f408a388c2ca44
unknown <xlinfr@NATGVC066.gukonto.gu.se>
2015-11-18 20:40:40 +0100

    Wrapper update. Now gridcode is not used if only one metfile is used. New output columns.

--- SuewsWrapper.py
+++ SuewsWrapper.py
@@ -1,10 +1,10 @@
 __author__ = 'xlinfr'
 
-import Suews_wrapper_v10
+import Suews_wrapper_v12
 import os
 import Tkinter
 import FileDialog
 
 # working_path = os.path.dirname(__file__)
 working_path = os.getcwd()
-Suews_wrapper_v10.wrapper(working_path)
+Suews_wrapper_v12.wrapper(working_path)

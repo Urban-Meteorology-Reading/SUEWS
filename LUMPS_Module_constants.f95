b3b5a454da97fc502644533d2cad1ab276df885d
HelenWard <h.c.ward@reading.ac.uk>
2017-02-08 10:43:50 +0000

    Bug fixed in running-mean OHM (and AnOHM) calculations

--- LUMPS_Module_constants.f95
+++ LUMPS_Module_constants.f95
@@ -836,7 +836,8 @@ MODULE data_in
        SMDMethod,&           !Use modelled (0) or observed(1,2) soil moisture
        WaterUseMethod,&            !Use modelled (0) or observed (1) water use
        RoughLenMomMethod,&              !Defines method for calculating z0 & zd
-       Diagnose              !Set to 1 to get print-out of model progress
+       Diagnose,&             !Set to 1 to get print-out of model progress
+       DiagQS
 
   ! ---- Model options currently set in model, but may be moved to RunControl at a later date
   INTEGER:: AlbedoChoice,&         !No additional albedo varaition (0); zenith angle calculation (1)

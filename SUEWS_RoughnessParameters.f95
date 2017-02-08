3c61ccd202e94b3082ac71030428775ca62f2f29
HelenWard <h.c.ward@reading.ac.uk>
2017-02-08 13:25:29 +0000

    improved error handling. — Helen

--- SUEWS_RoughnessParameters.f95
+++ SUEWS_RoughnessParameters.f95
@@ -1,8 +1,8 @@
 SUBROUTINE RoughnessParameters(Gridiv)
-  ! Last modified by HCW 03 Mar 2015
   ! Get surface covers and frontal area fractions (LJ 11/2010)
-  ! sg feb 2012 -- made separate subroutine
-  !INPUT: id  Day index
+  ! Last modified by HCW 08 Feb 2017 - fixed bug in Zh between grids, added default z0m, zdm
+  !                  HCW 03 Mar 2015
+  !                  sg feb 2012 - made separate subroutine
   !--------------------------------------------------------------------------------
   USE data_in
   USE gis_data
@@ -15,30 +15,52 @@ SUBROUTINE RoughnessParameters(Gridiv)
   IMPLICIT NONE
   INTEGER :: Gridiv
 
+  REAL(KIND(1D0)):: z0m4Paved,z0m4Grass,z0m4BSoil,z0m4Water   !Default values for roughness lengths [m]
+  
+  ! Set default values (using Moene & van Dam 2013, Atmos-Veg-Soil Interactions, Table 3.3)
+  Z0m4Paved = 0.003 !estimate 
+  Z0m4Grass = 0.02
+  Z0m4BSoil = 0.002
+  Z0m4Water = 0.0005
+  
   !------------------------------------------------------------------------------
-  !If total area of buildings and trees is larger than zero
-  IF (areaZh/=0)THEN
-     !Zh=bldgH*sfr(BldgSurf)/areaZh+treeH*sfr(ConifSurf)/areaZh+treeH*(1-porosity(id))*sfr(DecidSurf)/areaZh
+  !If total area of buildings and trees is larger than zero, use tree heights and building heights to calculate zH
+  IF (areaZh/=0) THEN
      Zh=bldgH*sfr(BldgSurf)/areaZh + EveTreeH*sfr(ConifSurf)/areaZh + DecTreeH*(1-porosity(id))*sfr(DecidSurf)/areaZh
+  ELSE
+     Zh=0   !Set Zh to zero if areaZh = 0
   ENDIF
-
-  !Calculate Z0m and Zdm depending on the Z0 method
-  IF(RoughLenMomMethod==2) THEN  !Rule of thumb (G&O 1999)
-     Z0m=0.1*Zh
-     Zdm=0.7*Zh
-  ELSEIF(RoughLenMomMethod==3)THEN !MacDonald 1998
-     IF (areaZh/=0)THEN  !Plan area fraction
-        !planF=FAIBldg*sfr(BldgSurf)/areaZh+FAItree*sfr(ConifSurf)/areaZh+FAItree*(1-porosity(id))*sfr(DecidSurf)/areaZh
-        planF=FAIBldg*sfr(BldgSurf)/areaZh + FAIEveTree*sfr(ConifSurf)/areaZh + FAIDecTree*(1-porosity(id))*sfr(DecidSurf)/areaZh
-     ELSE
-        planF=0.00001
-        Zh=1
+  
+  IF(Zh/=0)THEN
+     !Calculate Z0m and Zdm depending on the Z0 method
+     IF(RoughLenMomMethod==2) THEN  !Rule of thumb (G&O 1999)
+        Z0m=0.1*Zh
+        Zdm=0.7*Zh
+     ELSEIF(RoughLenMomMethod==3)THEN !MacDonald 1998
+        IF (areaZh/=0)THEN  !Plan area fraction
+           !planF=FAIBldg*sfr(BldgSurf)/areaZh+FAItree*sfr(ConifSurf)/areaZh+FAItree*(1-porosity(id))*sfr(DecidSurf)/areaZh
+           planF=FAIBldg*sfr(BldgSurf)/areaZh + FAIEveTree*sfr(ConifSurf)/areaZh + FAIDecTree*(1-porosity(id))*sfr(DecidSurf)/areaZh
+        ELSE
+           planF=0.00001
+           Zh=1
+        ENDIF
+        Zdm=(1+4.43**(-sfr(BldgSurf))*(sfr(BldgSurf)-1))*Zh
+        Z0m=((1-Zdm/Zh)*EXP(-(0.5*1.0*1.2/0.4**2*(1-Zdm/Zh)*planF)**(-0.5)))*Zh
      ENDIF
-
-     Zdm=(1+4.43**(-sfr(BldgSurf))*(sfr(BldgSurf)-1))*Zh
-     Z0m=((1-Zdm/Zh)*EXP(-(0.5*1.0*1.2/0.4**2*(1-Zdm/Zh)*planF)**(-0.5)))*Zh
+  ELSEIF(Zh==0)THEN   !If zh calculated to be zero, set default roughness length and displacement height
+     IF(areaZh /= 0) CALL ErrorHint(15,'In SUEWS_RoughnessParameters.f95, zh = 0 m but areaZh > 0',zh,areaZh,notUsedI)
+     !Estimate z0 and zd using default values and surfaces that do not contribute to areaZh
+     IF(areaZh /= 1)THEN
+        z0m = (z0m4Paved*sfr(PavSurf) + z0m4Grass*sfr(GrassSurf) + z0m4BSoil*sfr(BSoilSurf) + z0m4Water*sfr(WaterSurf))/(1-areaZh)
+        zdm = 0
+        CALL ErrorHint(15,'Setting z0m and zdm using default values',z0m,zdm,notUsedI)
+     ELSEIF(areaZh==1)THEN  !If, for some reason, Zh = 0 and areaZh == 1, assume height of 10 m and use rule-of-thumb
+        z0m = 1
+        zdm = 7
+        CALL ErrorHint(15,'Assuming mean height = 10 m, Setting z0m and zdm to default value',z0m,zdm,notUsedI)   
+     ENDIF 
   ENDIF
-
+     
   ZZD=Z-zdm
 
   ! Error messages if aerodynamic parameters negative

b754f72b2d8bf4cdb16922b357307a149a5e4bec
Ting Sun <sunting.05@gmail.com>
2017-10-22 21:03:40 +0100

    #f2py: improved `make` for f2py generation

--- f2py/make.sh
+++ f2py/make.sh
@@ -8,6 +8,9 @@
 export CFLAGS='-fno-strict-aliasing -fno-common -dynamic -g -O2 -DNDEBUG -g -fwrapv -O3 -Wall -Wstrict-prototypes -fbracket-depth=1024'
 export FFLAGS='-g -pg -Wall -Wtabs -fbounds-check -cpp -Wno-unused-dummy-argument -Wno-unused-variable -fbacktrace -ffpe-trap=zero,overflow,underflow,invalid,denormal'
 
+# clean existing built modules
+rm SUEWS_driver.so*
+
 # compile SUEWS with Makefile
 cd ..
 make clean
@@ -21,7 +24,9 @@ ln -sf ../*.o .
 
 # remove duplicate SUEWS_driver to avoid symbol conflict
 rm SUEWS_driver.o
+
 # direct compilation without f2py header file
+# --debug-capi provides more verbose information when calling C-API: useful for debugging
 # f2py -m SUEWS_driver -c --debug-capi ../SUEWS_driver.f95 *.o
 f2py -m SUEWS_driver -c ../SUEWS_driver.f95 *.o
 

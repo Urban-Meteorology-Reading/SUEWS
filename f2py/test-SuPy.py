c8ebb627766cac5043b389d382439fa1ac4c273d
Ting Sun <sunting.05@gmail.com>
2018-03-05 11:36:52 +0000

    fixed a bug in loading json files.

--- f2py/test-SuPy.py
+++ f2py/test-SuPy.py
@@ -43,39 +43,64 @@ df_output, df_state = sp.run_suews_df(df_forcing_part, df_state_init)
 # post-processing of model ouptuts:
 # convert dict of raw output to easier DataFrame:
 # {grid: Dataframe by group ({'SUEWS','ESTM','snow'})}
-df_output = sp.pack_df_output(dict_output)
-df_state = sp.pack_df_grid(dict_state)
+# df_output = sp.pack_df_output(dict_output)
+# df_state = sp.pack_df_grid(dict_state)
 
 
-# plot some variables
+# %% plot some variables
+idx = pd.IndexSlice
 # output
-xx = df_output.loc[1, 'SUEWS'].loc[:, ['QS', 'QN']].plot.line()
-plt.show(xx)
-
-
-# state variable
-yy = df(df_state.loc[1, 'soilmoist'][:, [3, 2]]).plot()
-plt.show(yy)
-
-
-# load observations
-list_file_Obs = glob.glob(os.path.join(
-    dir_start, 'Obs/{}*txt'.format(filecode)))
-res_obs = pd.read_table(list_file_Obs[0])
-res_obs.rename(columns={'dectime': 'Dectime', 'QgHFP': 'QS_obs'}, inplace=True)
-
-
-# comparison
-# specify the variable for comparison
-res_sim = df_output.loc[1, 'SUEWS'].loc[:, ['Dectime', 'QS']]
-res_sim.rename(columns={'QS': 'QS_sim'}, inplace=True)
-# merge two DF's
-res_comp = res_obs.copy()
-res_comp = res_obs.merge(res_sim, on='Dectime')
-res_comp.head()
-res_comp.plot(x='Dectime', y=['QS_obs', 'QS_sim'],
-              kind='line', figsize=(10.5, 3))
+xx = df_output.loc[idx[:, 1], [('datetime', 'Dectime'),
+                               ('SUEWS', 'QS'), ('SUEWS', 'QN')]]
+xx.columns = xx.columns.droplevel()
+xx.plot(x='Dectime')
 plt.show()
 
+# state variable TODO: df_state
+idx = pd.IndexSlice
+
+df_state_time = pd.concat(
+    [df_state,
+     df_output.loc[:, ('datetime', 'Dectime')].rename('Dectime')],
+    axis=1).dropna()
+# entry with single value
+y1 = df_state_time.loc[idx[:, 1], :].plot(
+    y='aerodynamicresistancemethod', x='Dectime')
+plt.show()
+# entry with value arrays
+y2 = df_state_time.loc[:, ['Dectime', 'soilmoist']]
+y2 = y2.join(y2['soilmoist'].apply(pd.Series), rsuffix='ss_').drop(
+    columns=['soilmoist']).loc[:,['Dectime',1,3]].plot(x='Dectime')
+plt.show()
+
+y3 = df_state_time.loc[100:, ['Dectime', 'qn1_av_store']]
+y3['qn1_av_store'] = y3['qn1_av_store'].apply(np.mean)
+y3=y3.plot(x='Dectime')
+plt.show()
+
+
+# %% comparison
+# load observations
+list_file_Obs = glob.glob(
+    os.path.join(dir_start,'Obs', '{}*txt'.format(filecode)))
+res_obs = pd.read_table(list_file_Obs[0], delim_whitespace=True)
+res_obs.columns = ['Dectime', 'QS_obs']
+# res_obs.rename(columns=, inplace=True)
+
+# specify the variable for comparison
+grid = df_output.index[0][1]
+res_sim = df_output.loc[idx[:, grid], [
+    ('datetime', 'Dectime'), ('SUEWS', 'QS'), ('SUEWS', 'QN')]].copy()
+res_sim.columns = res_sim.columns.droplevel()
+res_sim.rename(columns={'QS': 'QS_sim'}, inplace=True)
+# merge two DF's
+res_comp = res_sim.copy()
+res_comp = res_comp.merge(res_obs, left_on='Dectime',
+                          right_on='Dectime', how='inner')
+# res_comp.head()
+plt.close('all')
+res_comp.loc[:].plot(x='Dectime', y=['QS_obs', 'QS_sim'],
+                     kind='line', figsize=(8, 3), style=['o'])
+plt.show()
 
 #

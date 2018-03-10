36148362912f6bb7476d218927ecdeba72b76965
Ting Sun <sunting.05@gmail.com>
2018-03-10 14:57:33 +0000

    added a working version for shipping library-free (almost) package.

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
-from setuptools import setup, Distribution
+# from setuptools import setup, Distribution
+from setuptools import Distribution
+from numpy.distutils.core import Extension, setup
 import platform
+import glob
 
 
 def readme():
@@ -24,6 +27,22 @@ elif sysname == 'Darwin':
 else:
     lib_name = 'SUEWS_driver.so'
 
+target_f95 = ['SUEWS-AnOHM/LUMPS_Module_constants.f95',
+              'SUEWS-AnOHM/SUEWS_driver.f95']
+other_f95 = set(glob.glob('SUEWS-AnOHM/*f95')) - \
+    set(target_f95) - set(['SUEWS-AnOHM/SUEWS_C_wrapper.f95'])
+other_f95 = list(other_f95)
+other_obj = [f.replace('.f95', '.o') for f in other_f95]
+src_f95 = target_f95 + other_f95
+print src_f95,other_obj
+ext_modules = [
+    Extension('SUEWS_driver',
+              target_f95,
+              # src_f95,
+              extra_f90_compile_args=['-cpp'],
+              # library_dirs=['./SUEWS-AnOHM'],
+              extra_objects=other_obj,
+              extra_link_args=['-static'])]
 
 setup(name='supy',
       version='0.1.3a7',
@@ -35,9 +54,13 @@ setup(name='supy',
       license='GPL-V3.0',
       packages=['supy'],
       package_data={
-          'supy': [lib_name, '*.json']
+          'supy': [
+              # lib_name,
+           '*.json'
+          ]
       },
       distclass=BinaryDistribution,
+      ext_modules=ext_modules,
       install_requires=[
           'numpy',
           'pandas',

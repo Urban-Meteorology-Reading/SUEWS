501d1098bed9779d0d5ee8957107ca135d4d2daa
Ting Sun <sunting.05@gmail.com>
2018-03-11 21:12:29 +0000

    resolve case issues on different platforms.

--- SuPy/SuPy_module.py
+++ SuPy/SuPy_module.py
@@ -125,6 +125,8 @@ dict_Code2File.update(dict_varSiteSelect2File)
 # process RunControl.nml
 # this function can handle all SUEWS nml files
 def load_SUEWS_nml(xfile):
+    # remove case issues
+    xfile=path_insensitive(xfile)
     df = pd.DataFrame(f90nml.read(xfile))
     return df
 
@@ -137,6 +139,8 @@ def load_SUEWS_RunControl(xfile):
 
 # load all tables (xgrid.e., txt files)
 def load_SUEWS_table(fileX):
+    # remove case issues
+    fileX=path_insensitive(fileX)
     rawdata = pd.read_table(fileX, delim_whitespace=True,
                             comment='!', error_bad_lines=True,
                             skiprows=1, index_col=0).dropna()
@@ -1177,6 +1181,74 @@ def pack_df_state(dict_state):
 
 ##############################################################################
 # auxiliary functions
+# resolve path case issues
+def path_insensitive(path):
+    """
+    Get a case-insensitive path for use on a case sensitive system.
+
+    >>> path_insensitive('/Home')
+    '/home'
+    >>> path_insensitive('/Home/chris')
+    '/home/chris'
+    >>> path_insensitive('/HoME/CHris/')
+    '/home/chris/'
+    >>> path_insensitive('/home/CHRIS')
+    '/home/chris'
+    >>> path_insensitive('/Home/CHRIS/.gtk-bookmarks')
+    '/home/chris/.gtk-bookmarks'
+    >>> path_insensitive('/home/chris/.GTK-bookmarks')
+    '/home/chris/.gtk-bookmarks'
+    >>> path_insensitive('/HOME/Chris/.GTK-bookmarks')
+    '/home/chris/.gtk-bookmarks'
+    >>> path_insensitive("/HOME/Chris/I HOPE this doesn't exist")
+    "/HOME/Chris/I HOPE this doesn't exist"
+    """
+
+    return _path_insensitive(path) or path
+
+
+def _path_insensitive(path):
+    """
+    Recursive part of path_insensitive to do the work.
+    """
+
+    if path == '' or os.path.exists(path):
+        return path
+
+    base = os.path.basename(path)  # may be a directory or a file
+    dirname = os.path.dirname(path)
+
+    suffix = ''
+    if not base:  # dir ends with a slash?
+        if len(dirname) < len(path):
+            suffix = path[:len(path) - len(dirname)]
+
+        base = os.path.basename(dirname)
+        dirname = os.path.dirname(dirname)
+
+    if not os.path.exists(dirname):
+        dirname = _path_insensitive(dirname)
+        if not dirname:
+            return
+
+    # at this point, the directory exists but not the file
+
+    try:  # we are expecting dirname to be a directory, but it could be a file
+        files = os.listdir(dirname)
+    except OSError:
+        return
+
+    baselow = base.lower()
+    try:
+        basefinal = next(fl for fl in files if fl.lower() == baselow)
+    except StopIteration:
+        return
+
+    if basefinal:
+        return os.path.join(dirname, basefinal) + suffix
+    else:
+        return
+
 
 # convert pandas structures to Python native structures
 def conv2PyData(df_x):

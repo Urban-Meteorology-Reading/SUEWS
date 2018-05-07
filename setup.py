b5990100fa0707b8529204aa126da7696f5be258
Ting Sun <sunting.05@gmail.com>
2018-05-07 14:54:47 +0100

    corrected Makefile configurations

--- setup.py
+++ setup.py
@@ -63,7 +63,7 @@ ext_modules = [
               extra_link_args=[(''if sysname == 'Linux' else '-static')])]
 
 setup(name='supy',
-      version='0.2rc1',
+      version='0.3',
       description='the SUEWS model that speaks python',
       long_description=readme(),
       url='https://github.com/sunt05/SuPy',

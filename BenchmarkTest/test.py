8d0246975b570ae7f1a52b3b742a3647a0c170b0
Ting Sun <sunting.05@gmail.com>
2018-05-14 17:07:49 +0100

    minor changes

---
+++ BenchmarkTest/test.py
@@ -0,0 +1,6 @@
+df_test_pass.loc[df_test_pass == 'fail']
+df_test_pass.loc[df_test_pass['result'] == 'fail'].index.tolist()
+f90nml.write({'ss': dict_initcond.update({'22': 34})}, 'fn_nml2.nml')
+os.getcwd()
+
+dict_initcond

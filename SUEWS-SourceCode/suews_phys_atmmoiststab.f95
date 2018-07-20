1b5319f1379588197bb5f5670b5105dc10cb0873
Ting Sun <sunting.05@gmail.com>
2018-07-20 14:00:18 +0100

    changed the file name cases to prevent issues during compilation across platforms.

---
+++ SUEWS-SourceCode/suews_phys_atmmoiststab.f95
@@ -0,0 +1,384 @@
+MODULE AtmMoistStab_module
+  IMPLICIT NONE
+
+CONTAINS
+  !.c!! For Lumps Version 2 - no stability calculations
+  ! Latent heat of sublimation when air temperature below zero added. LJ Nov 2012
+  ! explict interface added to all subroutines, TS 08 Aug 2017
+  SUBROUTINE LUMPS_cal_AtmMoist(&
+       Temp_C,Press_hPa,avRh,dectime,&! input:
+       lv_J_kg,lvS_J_kg,&! output:
+       es_hPa,Ea_hPa,VPd_hpa,VPD_Pa,dq,dens_dry,avcp,air_dens)
+
+    USE meteo,ONLY:&
+         sat_vap_press_x,spec_heat_beer,&
+         lat_vap,lat_vapSublim,spec_hum_def
+
+    IMPLICIT NONE
+    REAL(KIND(1d0))::vap_dens
+
+    REAL(KIND(1d0)),INTENT(in)::&
+         Temp_C,&
+         Press_hPa,&
+         avRh,dectime
+    REAL(KIND(1d0)),INTENT(out)::&
+         lv_J_kg,&!Latent heat of vaporization in [J kg-1]
+         lvS_J_kg,&!Latent heat of sublimation in J/kg
+         es_hPa,&!Saturation vapour pressure over water in hPa
+         Ea_hPa,&!Vapour pressure of water in hPa
+         VPd_hpa,& !vapour pressure deficit in hPa
+         VPD_Pa,& !vapour pressure deficit in Pa
+         dq,&!Specific humidity deficit in g/kg
+         dens_dry,& !Vap density or absolute humidity	 (kg/m3)
+         avcp,&!specific heat capacity in J kg-1 K-1
+         air_dens!Air density in kg/m3
+
+
+    REAL (KIND(1d0)),PARAMETER:: &
+                                !  comp          = 0.9995, &
+                                !  epsil         = 0.62197,&           !ratio molecular weight of water vapor/dry air (kg/mol/kg/mol)
+                                !  epsil_gkg     = 621.97, &           !ratio molecular weight of water vapor/dry air in g/kg
+                                !  dry_gas       = 8.31451,&           !Dry gas constant (J/k/mol)
+                                !  gas_ct_wat    = 461.05,&            !Gas constant for water (J/kg/K)
+                                !  molar         = 0.028965,&          !Dry air molar fraction in kg/mol
+                                !  molar_wat_vap = 0.0180153,&         !Molar fraction of water vapor in kg/mol
+         gas_ct_dry    = 8.31451/0.028965,&  !j/kg/k=dry_gas/molar
+         gas_ct_wv     = 8.31451/0.0180153 !j/kg/kdry_gas/molar_wat_vap
+    !  waterDens     = 999.8395            !Density of water in 0 cel deg
+    INTEGER::from=1
+
+    !Saturation vapour pressure over water in hPa
+    es_hPa = sat_vap_press_x(Temp_C,Press_hPa,from,dectime) ! dectime is more or less unnecessary here
+
+    !Vapour pressure of water in hPa
+    Ea_hPa=avRh/100*es_hPa
+
+    ! if(debug.and.dectime>55.13.and.dectime<55.2)write(35,*)'%',Temp_C
+
+    VPd_hpa=es_hPa-ea_hpa           !vapour pressure deficit in hPa
+    VPD_Pa=(es_hPa*100)-(Ea_hPa*100)!vapour pressure deficit in Pa
+
+    dq=spec_hum_def(vpd_hPa,Press_hPa) !Specific humidity deficit in g/kg
+
+    !Vap density or absolute humidity	 (kg/m3)
+    vap_dens=(Ea_hPa*100/((Temp_C+273.16)*gas_ct_wv))
+
+    !density Dry Air Beer(1990)	kg/m3
+    dens_dry=((Press_hPa-Ea_hPa)*100)/(gas_ct_dry*(273.16+Temp_C))
+
+    !Air density in kg/m3
+    air_dens=(Press_hPa*100)/(gas_ct_dry*(Temp_C+273.16))
+
+    !Calculate specific heat capacity in J kg-1 K-1
+    avcp=spec_heat_beer(Temp_C,avRh,vap_dens,dens_dry)
+
+    !Latent heat of vaporization in [J kg-1]
+    lv_J_kg=lat_vap(Temp_C,Ea_hPa,Press_hPa,avcp,dectime)
+
+    !Latent heat of sublimation in J/kg
+    IF(Temp_C<0.000) THEN
+       lvS_J_kg=lat_vapSublim(Temp_C,Ea_hPa,Press_hPa,avcp)
+    ELSE
+       lvS_J_kg = 2834000
+    ENDIF
+    !if(debug)write(*,*)lv_J_kg,Temp_C,'lv2'
+    IF(press_hPa<900) THEN
+       CALL ErrorHint(46, 'Function LUMPS_cal_AtmMoist',press_hPa,-55.55d0, -55)
+    ENDIF
+    RETURN
+  END SUBROUTINE LUMPS_cal_AtmMoist
+
+
+
+
+
+
+  !.c!! For Lumps Version 2 - no stability calculations
+  !==========================================================
+  !     Last change:
+  !     TS   08 Aug 2017: added explicit interface
+  !     TS   13 Jun 2017: corrections to the integral of stability functions
+  !     MH   12 Apr 2017: Stable limit to exit do-loop
+  !     LJ   25 Nov 2014: Limits for L
+  !     LJ   19 Feb 2010
+  !     SG   27 Mar 2000    4:44 pm
+  !     ust - friction velocity
+  !     L - monin obukhov stability length
+  !       Van Ulden & Holtslag (1985) JCAM: 24: 1196-1207
+
+  SUBROUTINE STAB_lumps(&
+       StabilityMethod,&! input
+       dectime,& !Decimal time
+       zzd,&     !Active measurement height (meas. height-displac. height)
+       z0m,&     !Aerodynamic roughness length
+       zdm,&     !Displacement height
+       avU1,&    !Average wind speed
+       Temp_C,&  !Air temperature
+       h_init,    & !Kinematic sensible heat flux [K m s-1] used to calculate friction velocity
+       L_MOD,& !Obukhov length ! output:
+       TStar,& !T*
+       UStar,& !Friction velocity
+       zL)!Stability scale
+
+    IMPLICIT NONE
+    INTEGER,INTENT(in):: StabilityMethod
+
+
+    REAL(KIND(1d0)),INTENT(in)::dectime !Decimal time
+    REAL(KIND(1d0)),INTENT(in)::zzd     !Active measurement height (meas. height-displac. height)
+    REAL(KIND(1d0)),INTENT(in)::z0m     !Aerodynamic roughness length
+    REAL(KIND(1d0)),INTENT(in)::zdm     !Displacement height
+    REAL(KIND(1d0)),INTENT(in)::avU1    !Average wind speed
+    REAL(KIND(1d0)),INTENT(in)::Temp_C    !Air temperature
+    REAL(KIND(1d0)),INTENT(in)::h_init    !Kinematic sensible heat flux [K m s-1] used to calculate friction velocity
+
+
+    REAL(KIND(1d0)),INTENT(out)::L_MOD!Obukhov length
+    REAL(KIND(1d0)),INTENT(out)::TStar!T*
+    REAL(KIND(1d0)),INTENT(out)::UStar!Friction velocity
+    REAL(KIND(1d0)),INTENT(out)::zL ! Stability scale
+    ! REAL(KIND(1d0)),INTENT(out)::psim   !Stability function of momentum
+
+    REAL(KIND(1d0))::G_T_K,&
+         KUZ,&
+         L_MOD_save,&
+         psim,&
+         z0l,&
+         psimz0,&
+         h
+    REAL(KIND(1d0)),PARAMETER :: &
+         k=0.4,&             !Von Karman's contant
+         grav=9.80665  !g - gravity - physics today august 1987
+
+    INTEGER,PARAMETER::notUsedI=-55
+    INTEGER :: i
+
+    LOGICAL :: debug=.FALSE.
+
+    G_T_K=Dectime
+
+    IF(debug) WRITE(*,*)StabilityMethod,z0m,avU1,h_init,UStar,L_MOD
+    G_T_K=(Grav/(Temp_C+273.16))*k !gravity constant/(Temperature*Von Karman Constant)
+    KUZ=k*AvU1                     !Von Karman constant*mean wind speed
+    IF(zzd<0) CALL ErrorHint(32,'Windspeed Ht too low relative to zdm [Stability calc]- values [z-zdm, zdm]',Zzd,zdm,notUsedI)
+
+    UStar=KUZ/LOG(Zzd/z0m)      !Initial setting of u* and calc. of L_MOD (neutral situation)
+    IF ( ABS(h_init)<0.001 ) THEN    ! prevent zero TStar
+       h=0.001
+    ELSE
+       h=h_init
+    END IF
+    TStar=(-H/UStar)
+    L_MOD=(UStar**2)/(G_T_K*TStar)
+
+
+    IF(LOG(zzd/z0m)<0.001000) CALL ErrorHint(17,'In stability subroutine, (z-zd) < z0.',zzd,z0m,notUsedI)
+    DO i=1,330 !Iteration starts
+       L_MOD_save=L_MOD
+       zL=zzd/L_MOD
+       z0L=z0m/L_MOD  !z0m roughness length
+
+       ! IF (z/L>2)THEN
+       !    CALL ErrorHint(73,'LUMPS_atmos_functions_stab.f95: stability scale (z/L), UStar',zL,UStar,notUsedI)
+       !    RETURN !MO-theory not necessarily valid above zL>2. Still causes problematic UStar values and correct limit might be 0.3.
+       !    !Needs more investigations.
+       ! END IF
+
+
+       psim=stab_fn_mom(StabilityMethod,zL,zL)
+       ! psimz0=stab_fn_mom(StabilityMethod,zL,z0L)
+       psimz0=stab_fn_mom(StabilityMethod,z0L,z0L)
+
+
+       UStar=KUZ/(LOG(Zzd/z0m)-psim+psimz0) !Friction velocity in non-neutral situation
+
+
+
+       TStar=(-H/UStar)
+       L_MOD=(UStar**2)/(G_T_K*TStar)
+
+       ! IF(UStar<0.001000)THEN       !If u* too small
+       !    UStar=KUZ/(LOG(Zzd/z0m))
+       !    CALL ErrorHint(30,'SUBROUTINE STAB_lumps:[ u*< 0.001] zL,dectime',zL,dectime,notUsedI)
+       !    CALL ErrorHint(30,'SUBROUTINE STAB_lumps:[ u*< 0.001] z0l,UStar',z0l,UStar,notUsedI)
+       !    CALL ErrorHint(30,'SUBROUTINE STAB_lumps:[ u*< 0.001] psim,psimz0',psim,psimz0,notUsedI)
+       !    CALL ErrorHint(30,'SUBROUTINE STAB_lumps:[ u*< 0.001] AVU1,log(zzd/z0m)',AVU1,LOG(zzd/z0m),notUsedI)
+       !
+       !    ! RETURN
+       ! ENDIF
+
+       IF(ABS(L_MOD_save-L_MOD)<0.01)THEN
+          IF (ABS(L_MOD)>1e6) L_MOD = L_MOD/ABS(L_MOD)*1e6
+          ! RETURN
+          CONTINUE
+       ENDIF
+    ENDDO
+
+    ! limit zL to be with [-10,2]
+    IF ( zL<-5 .OR. zL>2 ) THEN
+       zL=MIN(2.,MAX(-5.,zL))
+       ! limit other output varialbes as well as z/L
+       L_MOD=zzd/zL
+       z0L=z0m/L_MOD
+       psim=stab_fn_mom(StabilityMethod,zL,zL)
+       ! psimz0=stab_fn_mom(StabilityMethod,zL,z0L)
+       psimz0=stab_fn_mom(StabilityMethod,z0L,z0L)
+       UStar=KUZ/(LOG(Zzd/z0m)-psim+psimz0)
+       TStar=(-H/UStar)
+    END IF
+
+
+
+
+  END SUBROUTINE STAB_lumps
+
+  !==================================================================
+
+  FUNCTION stab_fn_mom(StabilityMethod,zL,zL_f) RESULT(psym)
+    !     StabilityMethod = 1-4 -
+    !     PSYM - stability FUNCTION for momentum
+    !Modified by LJ Mar 2010
+    !Input:Used stability method, stability (z-d)/L, zeta (either (z-d)/L or z0/L)
+
+    ! USE mod_z
+    ! USE mod_k
+
+    IMPLICIT NONE
+    REAL(KIND(1d0)),PARAMETER :: neut_limit=0.001000 !Limit for neutral stability
+
+
+    REAL (KIND(1d0)):: piover2,psym,zL,zL_f,x,x2
+    INTEGER ::StabilityMethod
+
+    PIOVER2=ACOS(-1.)/2.
+    !PRINT*,StabilityMethod,zL,"stab_fn_mom:"
+    IF(ABS(zL)<neut_limit) THEN
+       psym=0
+    ELSEIF(zL<-neut_limit) THEN    !Unstable
+
+       IF(StabilityMethod==1)THEN     !    Jensen et al 1984 - Van Ulden & Holtslag (1985) p 1206&
+          psym=((1.-16.*zL_f)**0.25)-1
+       ELSEIF(StabilityMethod==2) THEN !Dyer (1974)(1-16z/L)**.25' k=0.41  mod. Hogstrom (1988)v15.2
+          X=(1.-(15.2*zL_f))**0.25
+          X2=LOG((1+(X**2.))/2.)
+          PSYM=(2.*LOG((1+X)/2.))+X2-(2.*ATAN(X))+PIOVER2
+       ELSEIF(StabilityMethod==3)THEN !     campbell & norman eqn 7.26
+          psym=0.6*(2)*LOG((1+(1-16*zL_f)**0.5)/2)
+       ELSEIF(StabilityMethod==4) THEN !Businger et al (1971) modifed  Hogstrom (1988)
+          x=(1-19.3*zL_f)**(-0.25)
+          X2=LOG((1+(X**2.))/2.)
+          PSYM=(2.*LOG((1+X)/2.))+X2-(2.*ATAN(X))+PIOVER2
+       ELSEIF(StabilityMethod==7) THEN ! Dyer & Bradley (1982) (1-28z/L)**.25' k=0.4
+          X=(1+(28.*zL_f))**0.25
+          X2=LOG((1+X**2.)/2.)
+          PSYM=(2.*LOG((1+X)/2.))+X2-(2.*ATAN(X))+PIOVER2
+       ELSEIF(StabilityMethod==5)THEN ! Zilitinkevich & Chalikov (1968) modified Hogstrom (1988)
+          IF(zL_f>=-0.16)THEN
+             x=1+1.38*zL_f
+          ELSE
+             x=0.42*(-1)*zL_f**0.333
+          ENDIF
+          X2=LOG((1+(X**2.))/2.)
+          PSYM=(2.*LOG((1+X)/2.))+X2-(2.*ATAN(X))+PIOVER2
+
+       ELSEIF(StabilityMethod==6)THEN !     Foken and Skeib (1983)
+          IF(zL_f>=0.06)THEN
+             x=1
+          ELSE
+             x=((-1)*zL_f/0.06)**0.25
+          ENDIF
+          X2=LOG((1+(X**2.))/2.)
+          PSYM=(2.*LOG((1+X)/2.))+X2-(2.*ATAN(X))+PIOVER2
+       ENDIF
+
+    ELSEIF(zL>neut_limit) THEN            !Stable
+
+       IF(StabilityMethod==1)THEN         !Dyer (1974) k=0.35 x=1+5*zL Mod. Hogstrom (1988)
+          psym=(-4.8)*zL_f
+       ELSEIF(StabilityMethod==2)THEN     !Van Ulden & Holtslag (1985) p 1206
+          IF ( zL_f >1000. ) THEN
+             zL_f=1000.
+          END IF
+          PSYM=(-17.*(1.-EXP(-0.29*zL_f)))
+       ELSEIF(StabilityMethod==4)THEN ! Businger et al (1971) modifed  Hogstrom (1988)
+          ! psym=1+6*zL_f  ! this is NOT the integral form but the stability function, TS 13 Jun 2017
+          psym=(-6)*zL_f   ! this is the integral form, TS 13 Jun 2017
+       ELSEIF(StabilityMethod==3)THEN ! campbell & norman eqn 7.27 p 97
+          psym=(-6)*LOG(1+zL_f)
+
+       ENDIF
+    ENDIF
+    RETURN
+  END FUNCTION stab_fn_mom
+
+  !_______________________________________________________________
+  !
+  ! PSYH - stability function for heat
+  FUNCTION stab_fn_heat(StabilityMethod,zL,zL_f) RESULT (psyh)
+    ! USE mod_k
+    IMPLICIT NONE
+    REAL(KIND(1d0)),PARAMETER :: neut_limit=0.001000 !Limit for neutral stability
+
+    REAL (KIND(1d0)):: zL,zL_f,psyh,x
+    INTEGER :: StabilityMethod
+
+    IF(ABS(zL)<neut_limit)THEN      !Neutral
+       psyh=0
+    ELSEIF(zL<-neut_limit) THEN     ! Unstable
+       IF(StabilityMethod==3)THEN
+          !campbell & norman eqn 7.26
+          psyh=0.6*(2)*LOG((1+(1-16*zL_f)**0.5)/2)
+       ELSE
+
+          IF(StabilityMethod==4)THEN ! Businger et al (1971) modifed  Hogstrom (1988)
+             x=0.95*(1.-11.6*zL_f)**(-0.5)
+          ELSEIF(StabilityMethod==7) THEN ! QUESTION: Ref?
+             x=(1-(28.*zL))**0.25
+          ELSEIF(StabilityMethod==2)THEN ! Dyer 1974 X=(1.-(16.*zL))**(0.5)modified Hosgstrom
+             x=0.95*(1.-15.2*zL_f)**(-0.5)
+          ENDIF
+          PSYH=2*LOG((1+x**2)/2)
+       ENDIF
+
+    ELSE IF (zL>neut_limit) THEN    !Stable
+       IF ( zL<=1 ) THEN ! weak/moderate stable
+          IF(StabilityMethod==4)THEN !Businger et al (1971) modifed  Hogstrom (1988)
+             ! psyh=0.95+(7.8*zL_f) ! this is NOT the integral form but the stability function, TS 13 Jun 2017
+             psyh=(-7.8)*zL_f   ! this is the integral form, TS 13 Jun 2017
+          ELSE !Dyer (1974)  PSYH=(-5)*zL	modifed  Hogstrom (1988)
+             PSYH=(-4.5)*zL_f
+          ENDIF
+       ELSE !zL>1, very stable. otherwise psyh would be too large. TS 13 Jun 2017
+          ! adopt the form as Brutasert (1982) eqn 4.58. but following the coeffs. of the above eqns
+          IF(StabilityMethod==4)THEN !Businger et al (1971) modifed  Hogstrom (1988)
+             psyh=(-7.8)*(1+LOG(zL_f))
+          ELSE !Dyer (1974)  PSYH=(-5)*zL	modifed  Hogstrom (1988)
+             PSYH=(-4.5)*(1+LOG(zL_f))
+          ENDIF
+       END IF
+
+    ENDIF
+
+    RETURN
+  END FUNCTION stab_fn_heat
+  !--------------------------------------------------------------------------------
+  ! psys - roughness sublayer correction psi_*
+  !
+  !     Garratt (1980) QJRMS Appendix 1 p 815/816
+
+  FUNCTION stab_fn_rou(z,zstar) RESULT (psys)
+    IMPLICIT NONE
+    REAL(KIND(1d0))::alpha,zeta,z,psys,zstar,alpha1
+    !     z wind speed height - z_d
+    !     zstar height of the roughness sublayer
+    !     eqn (a4) using alpha=0.5 alpha1=0.7
+    alpha=0.5
+    alpha1=0.7
+    zeta=z/zstar
+    psys=(alpha-1)* LOG(zeta)-(alpha*alpha1)*(1-zeta)-(alpha*alpha1**2) &
+         *(1-zeta**2)/6.-(alpha*alpha1**3)*(1-zeta**3)/24.
+    RETURN
+  END FUNCTION stab_fn_rou
+
+
+END MODULE AtmMoistStab_module
---
+++ SUEWS-SourceCode/suews_phys_biogenco2.f95
@@ -0,0 +1,221 @@
+!========================================================================================
+! Created by HCW Aug 2016 to calculate biogenic component of CO2 flux.
+! This subroutine is still under development and in the equations there might be bugs and
+! the code is not well commented.
+!
+! Last modified:
+! MH 20 Jun 2017  - Tidied and renamed from SUEWS_CO2.f95 to SUEWS_CO2Biogen.f95
+! HCW 11 Apr 2017 - Tidied and merged with LJ code
+! LJ   6 Apr 2017 - Minimum limit for soil respiration with BiogenCO2Choice = 2 was set to 0.6 umol m-2 s-1
+!                 - Choice for non-rectancular hyperbola to calculate the biogenic CO2 flux added (BiogenCO2Choice = 2)
+!                    (Bellucco et al. 2017, Agric. Forest. Met. 236, 113-122).
+!                 - Both the "local Helsinki model" (BiogenCO2Choice = 2)) and "general model" BiogenCO2Choice = 3) are implemented
+!                 - Snow fraction added to the calculation of active vegetation fraction and the soil respiration
+!
+! To Do:
+!  - Active vegetation goes to zero with LAI minimum, but this needs to be changed so some minimum value
+!    especially in the case of evergreentrees
+!  - Move some of the parameters to input files
+!  - Now on weekend nighttime population is used throughout the day. Do we need extra column in SiteSelect for daytime weekend population?
+
+! EmissionsMethod:
+!  11-16 - Rectangular hyperbola (Ruimy, Schmid, Flanagan)
+!  21-26 - Non-rectangular hyperbola, Helsinki (Bellucco et al. 2017)
+!  31-36 - Non-rectangular hyperbola, general  (Bellucco et al. 2017)
+!========================================================================================
+SUBROUTINE CO2_biogen(EmissionsMethod,&
+     ivConif,ivDecid,ivGrass,ConifSurf,DecidSurf,GrassSurf,BSoilSurf,&
+     snowFrac,nsurf,NVegSurf,avkdn,Temp_C,sfr,LAI_id,LAIMin,LAIMax,&
+     alpha_bioCO2,beta_bioCO2,theta_bioCO2,alpha_enh_bioCO2,beta_enh_bioCO2,&
+     resp_a,resp_b,min_res_bioCO2,Fc_biogen,Fc_respi,Fc_photo,&
+     notUsed,notUsedI)
+
+  IMPLICIT NONE
+  INTEGER,INTENT(in):: EmissionsMethod
+  INTEGER,INTENT(in)::&
+       ivConif,ivDecid,ivGrass,ConifSurf,DecidSurf,GrassSurf,BSoilSurf,&
+       nsurf,nvegSurf,&
+       notUsedI
+  REAL(KIND(1d0)),INTENT(in)::&
+       avkdn,&
+       Temp_C,&
+       notUsed
+
+  REAL(KIND(1d0)),DIMENSION(nsurf),INTENT(in)::&
+       sfr,&   !Surface fractions [-]
+       snowFrac
+  REAL(KIND(1d0)),DIMENSION(nvegsurf),INTENT(in):: LAI_id
+  REAL(KIND(1d0)),DIMENSION(nvegsurf),INTENT(in)::&
+       LAIMin, LAIMax,&      ! [m2 m-2]
+                                !  BiogenCO2Code,&       !Biogenic CO2 Code for SUEWS_BiogenCO2.txt
+       alpha_bioCO2,&
+       beta_bioCO2,&
+       theta_bioCO2,&
+       alpha_enh_bioCO2,&
+       beta_enh_bioCO2,&
+       resp_a,&
+       resp_b,&
+       min_res_bioCO2
+  REAL(KIND(1D0)),INTENT(out):: &
+       Fc_biogen,&
+       Fc_respi,Fc_photo
+
+  INTEGER:: iv ! counter
+
+  REAL(KIND(1d0)):: &
+       PAR_umolm2s1,&
+       Bellucco2017_Pho,&     ! Photosynthesis (Bellucco et al. 2016)
+       Bellucco2017_Res,&     ! Respiration (Bellucco et al. 2016)
+       Bellucco2017_Res_surf,&! Respiration for each vegetated surface
+       VegFracSum             ! Sum of vegetation fractions without water. Could be moved elsewhere later.
+
+  REAL(KIND(1d0)),DIMENSION(nvegsurf)::&
+       active_veg_fr,&         ! Active vegetation fraction
+       Fc_photo_surf,&         ! Photosynthesis for each vegetated surface
+       Bellucco2017_Pho_surf ! Photosynthesis for each vegetated surface
+
+  REAL(KIND(1d0)),DIMENSION(nvegsurf)::&
+       alpha_bioCO2_v2,&
+       beta_bioCO2_v2,&
+       theta_bioCO2_v2
+
+  REAL(KIND(1d0)),PARAMETER :: &
+       JtoumolPAR = 4.6,&
+       KdntoPAR = 0.46
+
+  !-----------------------------------------------------------------------
+
+  ! Calculate PAR from Kdown ----------------------------------
+  PAR_umolm2s1 = JtoumolPAR * KdntoPAR * avKdn
+
+  VegFracSum = sfr(ConifSurf) + sfr(DecidSurf) + sfr(GrassSurf)
+
+  ! Calculate active vegetation surface -----------------------
+  !Now this is zero always when LAI in its minimum values. This needs to vary between the summer time maximum and some minimum value
+  !especially in the case of evergreen trees (i.e. in early March LAI can be in its minimum value, but air temperature and radiation
+  !such that uptake can take place)
+  DO iv=ivConif,ivGrass   !For vegetated surfaces. Snow included although quite often LAI will be in its minimum when snow on ground
+     active_veg_fr(iv) = (sfr(iv+2)*(1-snowFrac(iv+2)))*(LAI_id(iv)-LAIMin(iv))/(LAIMax(iv)-LAIMin(iv))
+  ENDDO
+  !DO iv=ivConif,ivGrass   !For vegetated surfaces. Snow is not included?
+  !   active_veg_fr(iv) = sfr(iv+2)*(LAI(id-1,iv)-LAIMin(iv))/(LAIMax(iv)-LAIMin(iv))
+  !ENDDO
+
+
+  IF(EmissionsMethod>=11 .AND. EmissionsMethod<=16) THEN   ! Rectangular hyperbola
+
+     ! Calculate carbon uptake due to photosynthesis -------------
+     Fc_photo = 0
+     DO iv=ivConif,ivGrass
+        Fc_photo_surf(iv) = -beta_bioCO2(iv)*alpha_bioCO2(iv)*PAR_umolm2s1/(alpha_bioCO2(iv)*PAR_umolm2s1 + beta_bioCO2(iv))
+        ! For active vegetation fraction only
+        Fc_photo = Fc_photo + Fc_photo_surf(iv)*active_veg_fr(iv)  !umol m-2 s-1
+     ENDDO
+
+  ELSEIF(EmissionsMethod>=21 .AND. EmissionsMethod<=26) THEN  !Local model, Bellucco et al. (2017)
+
+     ! Calculate carbon uptake due to photosynthesis -------------
+     Bellucco2017_Pho = 0
+     DO iv=ivConif,ivGrass
+        Bellucco2017_Pho_surf(iv) = -(1/(2*theta_bioCO2(iv))*(alpha_bioCO2(iv)*PAR_umolm2s1+beta_bioCO2(iv)- &
+             SQRT((alpha_bioCO2(iv)*PAR_umolm2s1+beta_bioCO2(iv))**2-4* &
+             alpha_bioCO2(iv)*beta_bioCO2(iv)*theta_bioCO2(iv)*PAR_umolm2s1)))
+        ! For active vegetation fraction only
+        Bellucco2017_Pho = Bellucco2017_Pho + Bellucco2017_Pho_surf(iv)*active_veg_fr(iv)
+     ENDDO
+
+     !Fc_photo = Bellucco2017_Pho*active_veg_fr(ConifSurf-2)+ &
+     !           Bellucco2017_Pho*active_veg_fr(DecidSurf-2)+ &
+     !           Bellucco2017_Pho*active_veg_fr(GrassSurf-2)
+
+     Fc_photo = Bellucco2017_Pho
+
+  ELSEIF(EmissionsMethod>=31 .AND. EmissionsMethod<=36) THEN  !General model, Bellucco et al. (2017)
+     ! Not currently recommended as includes also some anthropogenic impacts. Should maybe be separate from other biogen choices?
+     ! Alpha and beta calculated as a function of vegetation cover fraction
+
+     !Different alpha, beta and theta vegetation cover values should be same in BiogenCO2Method = 3.
+     IF(alpha_bioCO2(ivConif) == alpha_bioCO2(ivDecid) .AND. alpha_bioCO2(ivConif) == alpha_bioCO2(ivGrass) .AND. &
+          beta_bioCO2(ivConif) == beta_bioCO2(ivDecid) .AND. beta_bioCO2(ivConif) == beta_bioCO2(ivGrass) .AND. &
+          theta_bioCO2(ivConif) == theta_bioCO2(ivDecid) .AND. theta_bioCO2(ivConif) == theta_bioCO2(ivGrass)) THEN
+
+        !Because different alpha, beta and theta values are same - only one vegetation type is needed.
+        alpha_bioCO2_v2(ivConif) = alpha_bioCO2(ivConif) + alpha_enh_bioCO2(ivConif)* &
+             (sfr(ConifSurf)+sfr(DecidSurf)+sfr(GrassSurf)+sfr(BSoilSurf))      !umol CO2 umol photons-1
+        beta_bioCO2_v2(ivConif) = -beta_bioCO2(ivConif) + beta_enh_bioCO2(ivConif)* &
+             (sfr(ConifSurf)+sfr(DecidSurf)+sfr(GrassSurf)+sfr(BSoilSurf))     !umol m^-2 s^-1
+
+        !Photosynthesis
+        !Bellucco2017_Pho = -(1/(2*theta_bioCO2(ivConif))*(alpha_bioCO2(ivConif)*PAR_umolm2s1+beta_bioCO2(ivConif)- &
+        !                   sqrt((alpha_bioCO2(ivConif)*PAR_umolm2s1+beta_bioCO2(ivConif))**2-4* &
+        !                   alpha_bioCO2(ivConif)*beta_bioCO2(ivConif)*theta_bioCO2(ivConif)*PAR_umolm2s1)))
+
+        Bellucco2017_Pho = -(1/(2*theta_bioCO2(ivConif))*(alpha_bioCO2_v2(ivConif)*PAR_umolm2s1+beta_bioCO2_v2(ivConif)- &
+             SQRT((alpha_bioCO2_v2(ivConif)*PAR_umolm2s1+beta_bioCO2_v2(ivConif))**2-4* &
+             alpha_bioCO2_v2(ivConif)*beta_bioCO2_v2(ivConif)*theta_bioCO2(ivConif)*PAR_umolm2s1)))
+
+     ELSE !If values are not same, then weighted average is calculated.
+        CALL ErrorHint(74,'Check values in SUEWS_BiogenCO2.txt: ',notUsed,notUsed,notUsedI)
+
+        ! Weighted averages
+        alpha_bioCO2_v2(ivConif) = (alpha_bioCO2(ivConif)*sfr(ConifSurf)/VegFracSum + &
+             alpha_bioCO2(ivDecid)*sfr(DecidSurf)/VegFracSum&
+             + alpha_bioCO2(ivGrass)*sfr(GrassSurf)/VegFracSum) &
+             / (alpha_bioCO2(ivConif) + alpha_bioCO2(ivDecid) + alpha_bioCO2(ivGrass))
+        beta_bioCO2_v2(ivConif)  = (beta_bioCO2(ivConif)*sfr(ConifSurf)/VegFracSum + &
+             beta_bioCO2(ivDecid)*sfr(DecidSurf)/VegFracSum&
+             + beta_bioCO2(ivGrass)*sfr(GrassSurf)/VegFracSum )/ (beta_bioCO2(ivConif) + &
+             beta_bioCO2(ivDecid) + beta_bioCO2(ivGrass))
+        theta_bioCO2_v2(ivConif) = (theta_bioCO2(ivConif)*sfr(ConifSurf)/VegFracSum  + &
+             theta_bioCO2(ivDecid)*sfr(DecidSurf)/VegFracSum&
+             + theta_bioCO2(ivGrass)*sfr(GrassSurf)/VegFracSum )/( theta_bioCO2(ivConif) + &
+             theta_bioCO2(ivDecid) + theta_bioCO2(ivGrass))
+
+        ! Using weighted average values to calculate alpha and beta as a function of vegetation cover fraction
+        alpha_bioCO2_v2(ivConif) = alpha_bioCO2_v2(ivConif) + alpha_enh_bioCO2(ivConif)* &
+             (sfr(ConifSurf)+sfr(DecidSurf)+sfr(GrassSurf)+sfr(BSoilSurf))     !umol CO2 umol photons-1
+        beta_bioCO2_v2(ivConif) = -beta_bioCO2_v2(ivConif) + beta_enh_bioCO2(ivConif)* &
+             (sfr(ConifSurf)+sfr(DecidSurf)+sfr(GrassSurf)+sfr(BSoilSurf))     !umol m^-2 s^-1
+
+        !Photosynthesis
+        !Bellucco2017_Pho = -(1/(2*theta_bioCO2(ivConif))*(alpha_bioCO2(ivConif)*PAR_umolm2s1+beta_bioCO2(ivConif)- &
+        !                   sqrt((alpha_bioCO2(ivConif)*PAR_umolm2s1+beta_bioCO2(ivConif))**2-4* &
+        !                   alpha_bioCO2(ivConif)*beta_bioCO2(ivConif)*theta_bioCO2(ivConif)*PAR_umolm2s1)))
+        Bellucco2017_Pho = -(1/(2*theta_bioCO2_v2(ivConif))*(alpha_bioCO2_v2(ivConif)*PAR_umolm2s1+beta_bioCO2_v2(ivConif)- &
+             SQRT((alpha_bioCO2_v2(ivConif)*PAR_umolm2s1+beta_bioCO2_v2(ivConif))**2-4* &
+             alpha_bioCO2_v2(ivConif)*beta_bioCO2_v2(ivConif)*theta_bioCO2_v2(ivConif)*PAR_umolm2s1)))
+
+     ENDIF
+
+     ! Calculate carbon uptake due to photosynthesis -------------
+     Fc_photo = Bellucco2017_Pho*active_veg_fr(ConifSurf-2)+ &
+          Bellucco2017_Pho*active_veg_fr(DecidSurf-2)+ &
+          Bellucco2017_Pho*active_veg_fr(GrassSurf-2)
+
+  ENDIF
+
+  ! Calculate carbon uptake due to respiration -------------
+  Bellucco2017_Res = 0.0
+  Bellucco2017_Res_surf = 0.0
+  IF (VegFracSum>0.01) THEN
+     DO iv=ivConif,ivGrass
+        IF (sfr(2+iv)>0.005) THEN
+           Bellucco2017_Res_surf = MAX(min_res_bioCO2(iv), resp_a(iv)*EXP(resp_b(iv)*Temp_C))
+           ! For active vegetation fraction only
+           ! Bellucco2017_Res = Bellucco2017_Res + Bellucco2017_Res_surf(iv)*active_veg_fr(iv)
+           Bellucco2017_Res = Bellucco2017_Res + Bellucco2017_Res_surf * sfr(2+iv)/VegFracSum
+        ENDIF
+     ENDDO
+  ENDIF
+  !Fc_respi = Bellucco2017_Res * (sfr(ConifSurf)*(1-snowFrac(ConifSurf))+sfr(DecidSurf)*(1-snowFrac(DecidSurf))+ &
+  !                                  sfr(GrassSurf)*(1-snowFrac(GrassSurf))+sfr(BSoilSurf)*(1-snowFrac(BSoilSurf)))
+  Fc_respi = Bellucco2017_Res * (sfr(ConifSurf)+sfr(DecidSurf)+sfr(GrassSurf)+sfr(BSoilSurf))
+
+  ! Combine to find biogenic CO2 flux
+  Fc_biogen = Fc_photo + Fc_respi
+
+
+  RETURN
+
+ENDSUBROUTINE CO2_biogen
+!========================================================================================

649cc7b05ee481683f9a9feeaa49b2acfa75b09b
Ting Sun <sunting.05@gmail.com>
2018-08-01 21:27:47 +0100

    updated table converter and input files.

--- ReleaseRepo/InputTables/TableConverter/SUEWS_TableConverter.py
+++ ReleaseRepo/InputTables/TableConverter/SUEWS_TableConverter.py
@@ -2,23 +2,30 @@
 ########################################################
 # Table Converter for SUEWS
 # Ting Sun, ting.sun@reading.ac.uk
+# Yihao Tang, Yihao.Tang@student.reading.ac.uk
 # history:
 # TS, 13 Oct 2017: initial version
+# YT, 01 Jun 2018: added the chained conversion
 ########################################################
+from __future__ import division
 import numpy as np
 import os.path
 import os
 from fnmatch import fnmatch
 from shutil import copyfile
 import f90nml
+import shutil
+from collections import defaultdict
+import pandas as pd
+from heapq import heappop, heappush
 
 
 # ignore warnings raised by numpy when reading-in -9 lines
 import warnings
 warnings.filterwarnings('ignore')
-
-
 ########################################################
+
+
 # load the rule file
 rules = np.genfromtxt('rules.csv', dtype=np.ndarray, delimiter=',', names=True)
 
@@ -45,8 +52,9 @@ def rename_var(toFile, toVar, toCol, toVal):
         # generate headers
         header = np.array(dataX.dtype.names)
         header[header == toVar] = toVal
-        headerLine = ' '.join(str(i + 1)
-                              for i in np.arange(len(dataX[0]))) + '\n' + ' '.join(header)
+        headerLine = ' '.join(
+            str(i + 1)
+            for i in np.arange(len(dataX[0]))) + '\n' + ' '.join(header)
 
         # convert to a more handy array
         dataX = np.array(dataX.tolist()).astype(str)
@@ -75,7 +83,7 @@ def delete_var(toFile, toVar, toCol, toVal):
     if toFile.endswith('.nml'):
         delete_var_nml(toFile, toVar, toVal)
     else:
-        dataX = np.genfromtxt(toFile, dtype=np.ndarray,skip_header=1,
+        dataX = np.genfromtxt(toFile, dtype=np.ndarray, skip_header=1,
                               comments='!', invalid_raise=False)
 
         # convert to a more handy array
@@ -91,7 +99,7 @@ def delete_var(toFile, toVar, toCol, toVal):
         headerLine = ' '.join(str(i + 1)
                               for i in np.arange(len(dataX[0])))
         # NB: caveat: comment info will be dropped, needs to be recovered
-        np.savetxt(toFile, dataX, fmt='%s',header=headerLine,
+        np.savetxt(toFile, dataX, fmt='%s', header=headerLine,
                    footer='-9\n-9', comments='')
 
         # print toVar + ' has been deleted!'
@@ -132,8 +140,9 @@ def add_var(toFile, toVar, toCol, toVal):
         # print toVar,int(toCol) - 1
         header = np.insert(header, int(toCol) - 1, toVar)
         # print header
-        headerLine = ' '.join(str(i + 1)
-                              for i in np.arange(len(header))) + '\n' + ' '.join(header)
+        headerLine = ' '.join(
+            str(i + 1)
+            for i in np.arange(len(header))) + '\n' + ' '.join(header)
         # print dataX.shape
         # if toVar exists:
         if toVar in dataX.dtype.names:
@@ -142,7 +151,7 @@ def add_var(toFile, toVar, toCol, toVal):
             # convert to a more handy array
             # print np.array(dataX.tolist()).astype(str).shape
             dataX = np.array(dataX.tolist()).astype(str)
-            dataX = dataX.reshape((-1,len(header)-1))
+            dataX = dataX.reshape((-1, len(header) - 1))
             # construct new column
             colNew = np.empty(dataX.shape[0], dtype=np.object)
             # colNew[0] = toCol
@@ -174,33 +183,34 @@ def add_var_nml(toFile, toVar, toVal):
     nml.write(toFile, force=True)
 
 
-# change:
-# change a variable to a target value
-def change_var(toFile, toCol, toRow, toVal):
-    # if namelist:
-    if toFile.endswith('.nml'):
-        change_var_nml(toFile, toVar, toVal)
-    else:
-        # load original data from toFile
-        dataX = np.genfromtxt(toFile, dtype=np.ndarray, skip_header=1,
-                              comments='!', names=True, invalid_raise=False)
-        # generate headers
-        headerLine = ' '.join(
-            str(i + 1) for i in np.arange(len(dataX[0]))) + '\n' + ' '.join(dataX.dtype.names)
-
-        # convert to a more handy array
-        dataX = np.array(dataX.tolist()).astype(str)
-
-        # change
-        # print np.array(toRow)-1, np.array(toCol)-1
-        # print dataX[np.array(toRow)-1, np.array(toCol)-1]
-        dataX[np.array(toRow) - 1, np.array(toCol) - 1] = toVal
-        # print dataX[np.array(toRow)-1, np.array(toCol)-1]
-
-        # NB: caveat: comment info will be dropped, needs to be recovered
-        np.savetxt(toFile, dataX, fmt='%s', header=headerLine,
-                   footer='-9\n-9', comments='')
-        return
+# # change:
+# # change a variable to a target value
+# def change_var(toFile, toCol, toRow, toVal):
+#     # if namelist:
+#     if toFile.endswith('.nml'):
+#         change_var_nml(toFile, toVar, toVal)
+#     else:
+#         # load original data from toFile
+#         dataX = np.genfromtxt(toFile, dtype=np.ndarray, skip_header=1,
+#                               comments='!', names=True, invalid_raise=False)
+#         # generate headers
+#         headerLine = ' '.join(
+#             str(i + 1) for i
+#             in np.arange(len(dataX[0]))) + '\n' + ' '.join(dataX.dtype.names)
+#
+#         # convert to a more handy array
+#         dataX = np.array(dataX.tolist()).astype(str)
+#
+#         # change
+#         # print np.array(toRow)-1, np.array(toCol)-1
+#         # print dataX[np.array(toRow)-1, np.array(toCol)-1]
+#         dataX[np.array(toRow) - 1, np.array(toCol) - 1] = toVal
+#         # print dataX[np.array(toRow)-1, np.array(toCol)-1]
+#
+#         # NB: caveat: comment info will be dropped, needs to be recovered
+#         np.savetxt(toFile, dataX, fmt='%s', header=headerLine,
+#                    footer='-9\n-9', comments='')
+#         return
 
 
 def change_var_nml(toFile, toVar, toVal):
@@ -253,8 +263,11 @@ def change_var_nml(toFile, toVar, toVal):
 #     return
 
 
-def SUEWS_Converter(fromDir, toDir, fromVer, toVer):
+# a single conversion between two versions
+def SUEWS_Converter_single(fromDir, toDir, fromVer, toVer):
     # copy files in fromDir to toDir, only: *.nml, SUEWS_*.txt
+    if os.path.exists(toDir) is False:
+        os.mkdir(toDir)
     fileList = []
     for fileX in os.listdir(fromDir):
         if any(fnmatch(fileX, p) for p in ['SUEWS*.txt', '*.nml']):
@@ -265,15 +278,16 @@ def SUEWS_Converter(fromDir, toDir, fromVer, toVer):
 
     # list all files involved in the given conversion
     posRules = np.unique(
-        np.where(np.array(rules[['From', 'To']].tolist()) == [fromVer, toVer])[0])
+        np.where(
+            np.array(rules[['From', 'To']].tolist()) == [fromVer, toVer])[0])
     filesToConvert = np.unique(rules['File'][posRules])
 
     for fileX in filesToConvert:
         actionList = rules[posRules].compress(
             rules['File'][posRules] == fileX, axis=0)
-        actionList=np.array(actionList.tolist())[:,2:].astype('S140')
+        actionList = np.array(actionList.tolist())[:, 2:].astype('S140')
         # prepend toDir to fileX
-        actionList[:,1]=os.path.join(toDir, fileX)
+        actionList[:, 1] = os.path.join(toDir, fileX)
         # print actionList
         SUEWS_Converter_file(os.path.join(toDir, fileX), actionList)
 
@@ -306,16 +320,92 @@ def SUEWS_Converter_file(fileX, actionList):
 def SUEWS_Converter_action(action, toFile, var, col, val):
     print action, toFile, var, col, val
     actionFunc = {'Rename': rename_var, 'Delete': delete_var,
-                   'Add': add_var}
+                  'Add': add_var}
     actionFunc[action](toFile, var, col, val)
 
     print action + ' ' + var + ' for ' + toFile + ': done!'
     return
 
 
-#example:
-fromDir='Input2017a'
-toDir='2017c'
-fromVer='2017a'
-toVer='2017c'
-SUEWS_Converter(fromDir, toDir, fromVer, toVer)
+def dijkstra(edges, f, t):
+    g = defaultdict(list)
+    for l, r, c in edges:
+        g[l].append((c, r))
+    q, seen = [(0, f, ())], set()
+
+    while q:
+        (cost, v1, path) = heappop(q)
+
+        if v1 not in seen:
+            seen.add(v1)
+            path = (v1, path)
+            if v1 == t:
+                return (cost, path)
+            for c, v2 in g.get(v1, ()):
+                if v2 not in seen:
+                    heappush(q, (cost + c, v2, path))
+
+    return float("inf")
+
+
+def version_list(fromVer, toVer):
+    edges = []
+    a = pd.read_csv('rules.csv')
+    v_from = np.unique(a['From'])
+    for i in v_from:
+        df = a[a['From'] == i]
+        for k in np.unique(df['To']):
+            edges.append((i, k, 1))
+
+    s = dijkstra(edges, fromVer, toVer)
+    chain_ver = []
+    while s:
+        chain_ver.append(s[0])
+        s = s[1]
+    return chain_ver
+
+
+# a chained conversion across multiole versions
+def SUEWS_Converter_chain(fromDir, toDir, fromVer, toVer):
+    chain_ver = version_list(fromVer, toVer)
+    temDir_1 = temDir_2 = fromDir
+    i = chain_ver[0]
+# Create temporary folders
+    if os.path.exists('tem1') is False:
+        os.mkdir('tem1')
+    if os.path.exists('tem2') is False:
+        os.mkdir('tem2')
+
+# Indirect version conversion process
+    while i > 1:
+        if i % 2:
+            temDir_2 = 'tem2'
+            SUEWS_Converter_single(
+                temDir_1, temDir_2, chain_ver[i + 1], chain_ver[i])
+            temDir_1 = 'tem1'
+            # Remove input temporary folders
+            shutil.rmtree('tem1', ignore_errors=True)
+
+        else:
+            temDir_1 = 'tem1'
+            SUEWS_Converter_single(
+                temDir_2, temDir_1, chain_ver[i + 1], chain_ver[i])
+            temDir_2 = 'tem2'
+            # Remove input temporary folders
+            shutil.rmtree('tem2', ignore_errors=True)
+            # this loop always break in this part
+        i -= 1
+
+    SUEWS_Converter_single(temDir_1, toDir, chain_ver[2], chain_ver[1])
+    # Remove temporary folders
+    shutil.rmtree('tem1', ignore_errors=True)
+    shutil.rmtree('tem2', ignore_errors=True)
+
+
+if __name__ == '__main__':
+    # example:
+    fromDir = '2016a'
+    toDir = '2018a'
+    fromVer = '2016a'
+    toVer = '2018a'
+    SUEWS_Converter_chain(fromDir, toDir, fromVer, toVer)

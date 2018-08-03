3ff34033846a907a96e92c21afa316c4c52e5b79
Ting Sun <sunting.05@gmail.com>
2018-08-03 16:13:42 +0100

    cleaned the dependence of suews driver on the output module.

--- SUEWS-SourceCode/suews_ctrl_output.f95
+++ SUEWS-SourceCode/suews_ctrl_output.f95
@@ -23,12 +23,12 @@ MODULE ctrl_output
 
 
   USE allocateArray
-  USE cbl_module
+  ! USE cbl_module
   USE data_in
-  USE defaultNotUsed
-  USE ESTM_data
+  ! USE defaultNotUsed
+  ! USE ESTM_data
   USE gis_data
-  USE initial
+  ! USE initial
   USE sues_data
   USE time
   USE strings
@@ -1082,6 +1082,40 @@ CONTAINS
     nlines=nlines-1 ! skip header
   END FUNCTION count_lines
 
+  SUBROUTINE output_name_n(i,name,group,aggreg)
+    ! used by f2py module `SuPy` to handle output names
+    IMPLICIT NONE
+    ! the dimension is potentially incorrect,
+    ! which should be consistent with that in output module
+    INTEGER,INTENT(in) :: i
+    CHARACTER(len = 15),INTENT(out) :: name,group,aggreg
+
+    INTEGER :: nVar
+    n=SIZE(varList, dim=1)
+    IF ( i<nVar .AND.i>0  ) THEN
+       name   = TRIM(varList(i)%header)
+       group  = TRIM(varList(i)%group)
+       aggreg = TRIM(varList(i)%aggreg)
+    ELSE
+       name   = ''
+       group  = ''
+       aggreg = ''
+    END IF
+
+
+  END SUBROUTINE output_name_n
+
+
+  SUBROUTINE output_size(nVar)
+    ! used by f2py module `SuPy` to get size of the output list
+    IMPLICIT NONE
+    ! the dimension is potentially incorrect,
+    ! which should be consistent with that in output module
+    INTEGER,INTENT(out) :: nVar
+
+    nVar=SIZE(varList, dim=1)
+
+  END SUBROUTINE output_size
 
 
   !===========================================================================!

77e5ffa2d9eea4bb91b8f7302ec829a53c61d9d8
Ting Sun <sunting.05@gmail.com>
2018-10-30 20:40:09 +0000

    updated supy-driver

--- supy-driver/setup.py
+++ supy-driver/setup.py
@@ -102,7 +102,7 @@ ext_modules = [
               extra_link_args=[('' if sysname == 'Linux' else '-static')])]
 
 setup(name='suews_driver',
-      version=get_suews_version(ver_minor=7),
+      version=get_suews_version(ver_minor=9),
       description='the SUEWS driver driven by f2py',
       long_description=readme(),
       url='https://github.com/sunt05/SuPy',

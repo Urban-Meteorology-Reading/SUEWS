95f25f69b938a904d06377501729309fce36cf7d
Ting Sun <sunting.05@gmail.com>
2019-01-25 02:26:02 +0000

    improved variable naming consistency in snow module

--- SUEWS-SourceCode/suews_ctrl_const.f95
+++ SUEWS-SourceCode/suews_ctrl_const.f95
@@ -417,7 +417,7 @@ MODULE allocateArray
                                        Qm_melt, &         !Heat consumption by snow melt
                                        Qm_rain, &         !Heat by rain falling on snow
                                        rainOnSnow, &      !Liquid precipitation falling on snow ()
-                                       snowD, &
+                                       SnowPackLimit, &
                                        deltaQi
 
    REAL(KIND(1d0)), DIMENSION(nsurf):: snowPack, &        !Amount of snow on each surface in mm
@@ -1245,7 +1245,7 @@ MODULE snowMod
                      SnowAlbMax, &       !Maximum snow albedo
                      SnowDensMin, &      !Minimum density of snow
                      SnowDensMax, &      !Maximum density of snow
-                     SnowLimBuild, &     !Snow removal limits for roofs in mm)
+                     SnowLimBldg, &     !Snow removal limits for roofs in mm)
                      SnowLimPaved, &     !Snow removal limits for paved surfaces in mm)
                      swe, &        !Weighted snow water equivalent (in mm)
                      tau_a, &            !Time constans related to albedo change

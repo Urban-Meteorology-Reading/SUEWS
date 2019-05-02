cd38e3e64676aca1f4b895874ac564b0245d0d4d
natalieth <ntheeuwes@gmail.com>
2019-05-02 17:29:03 +0100

    bit of clean-up

--- BenchmarkTest/BaseRun/2018c/test.py
+++ BenchmarkTest/BaseRun/2018c/test.py
@@ -25,6 +25,17 @@ plt.pcolormesh(df.index,np.arange(0.,3,0.1),df.iloc[:,5:35].T,cmap='YlGnBu',vmin
 plt.xlabel('Month-day hour')
 plt.ylabel('$z/z_H$ [-]')
 plt.colorbar(label='U [m s$^{-1}$]')
+plt.draw()
+
+plt.figure()
+colormap = plt.cm.YlGnBu
+plt.gca().set_color_cycle([colormap(i) for i in np.linspace(0, 0.9, 30)])
+
+for x in np.arange(len(df.index)/4):
+    plt.plot(df.iloc[x,5:35].T,np.arange(0.,3,0.1))
+
+plt.xlabel('U [m s$^{-1}$]')
+plt.ylabel('$z/z_H$ [-]')
 plt.show()
 
 

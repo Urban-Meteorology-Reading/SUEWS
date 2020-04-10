7544485674857710f514207725a6719ba7f90903
Ting Sun <sunting.05@gmail.com>
2020-04-10 08:56:55 +0100

    update driver version

--- supy-driver/setup.py
+++ supy-driver/setup.py
@@ -147,7 +147,7 @@ ext_modules = [
 setup(
     name="supy_driver",
     # update version info here!
-    version=get_suews_version(ver_minor=4),
+    version=get_suews_version(ver_minor=6),
     description="the SUEWS driver driven by f2py",
     long_description=readme(),
     url="https://github.com/sunt05/SuPy",
